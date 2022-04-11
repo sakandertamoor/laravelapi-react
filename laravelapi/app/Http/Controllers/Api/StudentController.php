171c90581d9a3dfa55949ca58bd5a8af227961da
Sakander Tamoor <sikander.tamoor@gmail.com>
2022-04-11 11:03:11 +0500

    checking stash

--- laravelapi/app/Http/Controllers/Api/StudentController.php
+++ laravelapi/app/Http/Controllers/Api/StudentController.php
@@ -51,7 +51,7 @@ class StudentController extends Controller
             return response()->json([
                 'status' => 200,
                 'result' => $student,
-                'message' => 'Student Data Updated Successfully'
+                'message' => 'Student Data Updated Successfully '
             ]);
         }
     }
